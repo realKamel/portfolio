c886c4b1fd5c491196c488fa8071b72f85ced5f0
abdelrahman <abdo.aly.k.m@gmail.com>
2024-12-30 13:42:03 +0200

    adding main components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CVComponent } from './components/cv/cv.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'header', pathMatch: 'full' },
+	{ path: 'header', component: HeaderComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'skills', component: SkillsComponent },
+	{ path: 'projects', component: ProjectsComponent },
+	{ path: 'cv', component: CVComponent },
+	{ path: 'contact', component: ContactComponent },
+];

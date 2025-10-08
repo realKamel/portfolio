98645c6117cf41231577aeadcddf6320274d3378
Abdelrahman <abdo.aly.k.m@gmail.com>
2025-10-08 11:28:32 +0300

    main app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import './App.css'
-
-function App () {
-  return <p>Working</p>
+import "./App.css";
+import About from "./components/sections/About";
+import Contact from "./components/sections/Contact";
+import Experience from "./components/sections/Experience";
+import Footer from "./components/sections/Footer";
+import Hero from "./components/sections/Hero";
+import Projects from "./components/sections/Projects";
+import Skills from "./components/sections/Skills";
+import TechStack from "./components/sections/TechStack";
+function App() {
+	return (
+		<>
+			<Hero />
+			<About />
+			<Skills />
+			<Projects />
+			<Experience />
+			<TechStack />
+			<Contact />
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
